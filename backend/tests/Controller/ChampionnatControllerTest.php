fbb89ca01442ea0942757ef2330486a9bf4f9bba
Nassim <146091934+BUCHMULLER-Nassim-2326028b@users.noreply.github.com>
2026-02-12 19:36:14 +0100

    fix: test unitaires

--- backend/tests/Controller/ChampionnatControllerTest.php
+++ backend/tests/Controller/ChampionnatControllerTest.php
@@ -4,12 +4,18 @@ namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * Test du contrôleur ChampionnatController.
+ */
 final class ChampionnatControllerTest extends WebTestCase
 {
+    /**
+     * Teste que la page /championnat répond correctement.
+     */
     public function testIndex(): void
     {
         $client = static::createClient();
-        $client->request('GET', '/championnats');
+        $client->request('GET', '/championnat');
 
         self::assertResponseIsSuccessful();
     }
